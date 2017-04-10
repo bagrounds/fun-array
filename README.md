1beeb3b00630b6be2d11b86a48c873c5540b275b
bagrounds <bagrounds@gmail.com>
2017-04-09 20:13:12 -0700

    [MINOR][ADD] initial files and implementation (initial commit)

---
+++ README.md
@@ -0,0 +1,26 @@
+# [fun-array](https://bagrounds.gitlab.io/fun-array)
+
+Functions for working with arrays functionally
+
+[![build-status](https://gitlab.com/bagrounds/fun-array/badges/master/build.svg)](https://gitlab.com/bagrounds/fun-array/commits/master)
+[![coverage-report](https://gitlab.com/bagrounds/fun-array/badges/master/coverage.svg)](https://gitlab.com/bagrounds/fun-array/commits/master)
+[![license](https://img.shields.io/npm/l/fun-array.svg)](https://www.npmjs.com/package/fun-array)
+[![version](https://img.shields.io/npm/v/fun-array.svg)](https://www.npmjs.com/package/fun-array)
+[![downloads](https://img.shields.io/npm/dt/fun-array.svg)](https://www.npmjs.com/package/fun-array)
+[![downloads-monthly](https://img.shields.io/npm/dm/fun-array.svg)](https://www.npmjs.com/package/fun-array)
+[![dependencies](https://david-dm.org/bagrounds/fun-array/status.svg)](https://david-dm.org/bagrounds/fun-array)
+
+## [Test Coverage](https://bagrounds.gitlab.io/fun-array/coverage/lcov-report/index.html)
+
+## [API Docs](https://bagrounds.gitlab.io/fun-array/docs/index.html)
+
+## Dependencies
+
+### Without Tests
+
+![Dependencies](https://bagrounds.gitlab.io/fun-array/img/dependencies.svg)
+
+### With Tests
+
+![Test Dependencies](https://bagrounds.gitlab.io/fun-array/img/dependencies-test.svg)
+
---
+++ src/index.js
@@ -0,0 +1,178 @@
+/**
+ *
+ * @module fun-array
+ */
+;(function () {
+  'use strict'
+
+  /* imports */
+  var curry = require('fun-curry')
+
+  /* exports */
+  module.exports = {
+    reIndex: curry(reIndex),
+    of: of,
+    empty: empty,
+    concat: curry(concat),
+    map: curry(map),
+    ap: curry(ap),
+    get: curry(get),
+    set: curry(set),
+    filter: curry(filter),
+    reverse: reverse,
+    sort: curry(sort)
+  }
+
+  /**
+   *
+   * @function module:fun-array.reIndex
+   *
+   * @param {Array<Number>} indices - array of old indices in a new order
+   * @param {Array} source - to get values from
+   *
+   * @return {Array} in a new order
+   */
+  function reIndex (indices, source) {
+    return source.reduce(function (result, value, i) {
+      result[i] = typeof indices[i] === 'number' ? source[indices[i]] : value
+
+      return result
+    }, [])
+  }
+
+  /**
+   *
+   * @function module:fun-array.filter
+   *
+   * @param {Function} predicate - to determine value membership
+   * @param {Array} source - to get values from
+   *
+   * @return {Array} of values that passed predicate
+   */
+  function filter (predicate, source) {
+    return source.filter(predicate)
+  }
+
+  /**
+   *
+   * @function module:fun-array.ap
+   *
+   * @param {Array} functions - to apply
+   * @param {Array} source - to get value from
+   *
+   * @return {Array} [functions[0](source[0]), functions[1](source[1]), ...]
+   */
+  function ap (functions, source) {
+    return source.map(function (value, i) {
+      return (functions[i] || id)(value)
+    })
+  }
+
+  /**
+   *
+   * @function module:fun-array.map
+   *
+   * @param {Function} f - * -> *
+   * @param {Array} source - to map f over
+   *
+   * @return {Array} [f(source[1]), f(source[2]), ...]
+   */
+  function map (f, source) {
+    return source.map(f)
+  }
+
+  /**
+   *
+   * @function module:fun-array.get
+   *
+   * @param {Number} index - of value
+   * @param {Array} source - to get value from
+   *
+   * @return {*} value at key
+   */
+  function get (index, source) {
+    return source[index]
+  }
+
+  /**
+   *
+   * @function module:fun-array.set
+   *
+   * @param {Number} index - to set
+   * @param {*} value - to set
+   * @param {Array} source - to set value on
+   *
+   * @return {Array} copy of source object containing value at key
+   */
+  function set (index, value, source) {
+    var result = source.map(id)
+    result[index] = value
+
+    return result
+  }
+
+  /**
+   *
+   * @function module:fun-array.concat
+   *
+   * @param {Array} a1 - first array
+   * @param {Array} a2 - second array
+   *
+   * @return {Array} [...a1, ...a2]
+   */
+  function concat (a1, a2) {
+    return a1.concat(a2)
+  }
+
+  /**
+   *
+   * @function module:fun-array.empty
+   *
+   * @return {Array} []
+   */
+  function empty () {
+    return []
+  }
+
+  /**
+   *
+   * @function module:fun-array.of
+   *
+   * @param {*} value - to put in an array
+   *
+   * @return {Array} [value]
+   */
+  function of (value) {
+    return [value]
+  }
+
+  /**
+   *
+   * @function module:fun-array.reverse
+   *
+   * @param {Array} array - to reverse
+   *
+   * @return {Array} of values in reverse order
+   */
+  function reverse (array) {
+    return map(id, array).reverse()
+  }
+
+  /**
+   *
+   * @function module:fun-array.sort
+   *
+   * @param {Function} comparator - (a, b) => (a < b) ? -1 : (a == b) ? 0 : 1
+   * @param {Array} array - to sort
+   *
+   * @return {Array} of values sorted by comparator
+   */
+  function sort (comparator, array) {
+    return map(id, array).sort(comparator)
+  }
+
+  function id (x) {
+    return x
+  }
+})()
+
